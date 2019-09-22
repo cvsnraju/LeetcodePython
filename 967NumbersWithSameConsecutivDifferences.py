7625bcbf79c9d0e74a63a7573979da4805319659
Venkata Chiluvuri <venkatc@Venkats-MacBook-Pro.local>
2019-09-22 17:11:18 +0530

    solved

--- 967NumbersWithSameConsecutivDifferences.py
+++ 967NumbersWithSameConsecutivDifferences.py
@@ -1,8 +1,23 @@
-def NumbersWithSameConsecutiveDifference(N, K):
-
+def numsSameConsecDiff( N, K):
+    """
+    :type N: int
+    :type K: int
+    :rtype: List[int]
+    """
+    ans = {x for x in range(1, 10)}
     if N == 1:
-        return xrange(0,10)
-
-
-res = NumbersWithSameConsecutiveDifference(1,12)
+        ans.add(0)
+    for _ in range(N-1):
+        ans2 = set()
+        for x in ans:
+            d = x % 10
+            if d - K >= 0:
+                ans2.add(10*x + d-K)
+            if d + K <= 9:
+                ans2.add(10*x + d+K)
+        ans = ans2
+        if N == 1:
+            ans
+    return list(ans)
+res = numsSameConsecDiff(2,5)
 print(res)
