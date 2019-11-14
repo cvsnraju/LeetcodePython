be87c8b56917ae650dfa669c9737f7fd799314f8
Venkata Chiluvuri <csatyanarayanaraju@gmail.com>
2019-11-14 19:07:49 +0530

    697DegreeofanArray.py

---
+++ 697DegreeofanArray.py
@@ -0,0 +1,22 @@
+# 697. Degree of an Array
+# Easy
+
+# Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
+
+# Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
+
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        left, right, count = {}, {}, {}
+        for i, x in enumerate(nums):
+            if x not in left: left[x] = i
+            right[x] = i
+            count[x] = count.get(x, 0) + 1
+
+        ans = len(nums)
+        degree = max(count.values())
+        for x in count:
+            if count[x] == degree:
+                ans = min(ans, right[x] - left[x] + 1)
+
+        return ans
