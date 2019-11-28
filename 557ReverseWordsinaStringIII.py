7cd891be9b1c62760de7abda210f24772b20cf84
Venkata Chiluvuri <csatyanarayanaraju@gmail.com>
2019-11-28 23:02:42 +0530

    seperate,reverse and join

---
+++ 557ReverseWordsinaStringIII.py
@@ -0,0 +1,16 @@
+# Easy
+
+# Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+# Example 1:
+
+# Input: "Let's take LeetCode contest"
+# Output: "s'teL ekat edoCteeL tsetnoc"
+
+class Solution:
+  def reverseWords(self, s: str) -> str:
+    words=s.split()
+    newords=[]
+    for word in words:
+      newords.append(word[::-1])
+    return " ".join(newords)
